d4563dec6e755525b17fd239a2ba5501e3c5949f
unknown <shuhao.zhang@mail.mcgill.ca>
2021-09-05 13:22:24 -0400

    finish the twoSum exercise

---
+++ twoSum.js
@@ -0,0 +1,62 @@
+
+//Solution 1
+const twoSum_1 = function(array, targetSum){
+  let compliment = [];
+  let reminder;
+  for (let i = 0; i < array.length; i++){
+    compliment.push(targetSum - array[i]); 
+    // [7, 5, 14, 2, -1, 9, 11, 4]
+    // [3, 5, -4, 8, 11, 1, -1, 6]  
+    for (let j = 0; j < compliment.length; j++){
+      if(i != j){
+        if (array[i] == compliment[j]){
+          return [array[i],array[j]];
+        }
+      }
+    }
+  }
+}
+
+
+// Solution 2
+const twoSum_2 = function(array, targetSum){
+  let reminder;
+  for (let i = 0; i < array.length; i++){
+    reminder = targetSum - array[i];
+    if (array.indexOf(reminder) != i){
+     if (array.indexOf(reminder) != -1){
+        return [array[array.indexOf(reminder)],array[i]];
+     }
+   }
+  }
+}
+
+const twoSum_3 = function(array, targetSum){
+  let left = 0;
+  let right = array.length-1;
+  while(right > left){
+    if (array[right] + array[left] > targetSum){
+      right--;
+    }
+    else if (array[right] + array[left] < targetSum){
+      left ++;
+    }
+    else{
+      return [array[right],array[left]];
+    }
+    }
+  
+}
+/*
+Sample input
+array = [3, 5, -4, 8, 11, 1, -1, 6]
+targetSum = 10
+
+Sample output
+[-1, 11]
+*/
+const array = [3, 5, -4, 8, 11, 1, -1, 6];
+const targetSum = 10;
+console.log(twoSum_1(array,targetSum))
+console.log(twoSum_2(array,targetSum))
+console.log(twoSum_3(array,targetSum))
