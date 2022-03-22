079fc60a48e11aa57daee481dc585460b9f2bb09
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-03-21 22:15:03 -0400

    Time: 28 ms (94.16%), Space: 13.8 MB (67.33%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,9 +2,11 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        array = [1, 2]
+        array = [0] * n
+        array[0] = 1
+        array[1] = 2
         
-        for i in range(n - 2):
-            array.append(array[-1] + array[-2])
+        for i in range(2, n):
+            array[i] = array[i - 1] + array[i - 2]
         
-        return array[-1]
+        return array[n-1]
