d37a680dfd42f321aa31e3e38a00515ee28fe21b
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-04-04 11:08:30 -0400

    Time: 51 ms (30.78%), Space: 13.8 MB (79.32%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -7,19 +7,14 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
-        def traversal(root, res = []):
-            if root:
-                res.append(root.val)
-                traversal(root.left, res)
-                traversal(root.right, res)
-            else:
-                res.append(None)
-            return res
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
         
-        
-        resP = traversal(p, [])
-        resQ = traversal(q, [])
-        return resP == resQ
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
         
         
