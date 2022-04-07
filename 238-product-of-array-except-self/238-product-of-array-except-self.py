52874e181bd1ed1bc9cd77f06c9182addfb738c6
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-04-06 21:41:39 -0400

    Time: 265 ms (72.43%), Space: 20.8 MB (98.51%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,5 +1,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # function to check if there are two or more than two zeros
         def isTwoZeros(nums):
           count = 0
           for i in range(len(nums)):
@@ -11,12 +12,18 @@ class Solution:
         
         product = 1
         non_zero_product = 1
+        
+        # return a list of 0 if there are two or more zeros in the nums
         if isTwoZeros(nums):
           return [0]*len(nums)
+        
+        # calculate the product of every num in nums
         for i in range(len(nums)):
           if nums[i] != 0:
             non_zero_product *= nums[i]
           product *= nums[i]
+        
+        # distinguish product and non_zero_product to prevent zero division exception
         for i in range(len(nums)):
           if nums[i] == 0:
             nums[i] = non_zero_product
