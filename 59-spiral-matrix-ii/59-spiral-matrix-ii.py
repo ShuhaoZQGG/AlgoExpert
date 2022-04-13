1b49a12bd0cb8341e23a19bc33c5d19716ad7853
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-04-13 16:33:05 -0400

    Time: 39 ms (70.76%), Space: 13.8 MB (85.81%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -2,21 +2,29 @@ class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         matrix = [[0 for i in range(n)] for j in range(n)]
         counter = 1
-        for layer in range((n+1)//2):
-            for ptr in range(layer, n-layer):
-                matrix[layer][ptr] = counter
+        rowBegin = 0
+        rowEnd = n - 1
+        columnBegin = 0
+        columnEnd = n - 1
+        while (rowBegin <= rowEnd and columnBegin <= columnEnd):
+            for i in range(columnBegin, columnEnd + 1):
+                matrix[rowBegin][i] = counter
                 counter += 1
+            rowBegin += 1
+            
+            for i in range(rowBegin, rowEnd + 1):
+                matrix[i][columnEnd] = counter
+                counter += 1
+            columnEnd -= 1
 
             
-            for ptr in range(layer + 1, n-layer):
-                matrix[ptr][n-layer-1] = counter
+            for i in range(columnEnd, columnBegin - 1, -1):
+                matrix[rowEnd][i] = counter
                 counter += 1
+            rowEnd -= 1
             
-            for ptr in range(layer + 1, n-layer):
-                matrix[n - layer - 1][n - ptr - 1] = counter
+            for i in range(rowEnd, rowBegin - 1, -1):
+                matrix[i][columnBegin] = counter
                 counter += 1
-                
-            for ptr in range(layer + 1, n-layer-1):
-                matrix[n - ptr - 1][layer] = counter
-                counter += 1
+            columnBegin += 1
         return matrix
