1abfe734d64e8444d5601977d256e432ad9a0cfe
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-04-14 21:32:11 -0400

    Time: 228 ms (34.70%), Space: 14.1 MB (22.08%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left, right = 0, 0
+        subString = ''
+        counter = 0
+        hashmap = dict()
+        while left < len(s) and right < len(s):
+          if s[right] not in hashmap:
+            hashmap[s[right]] = 0
+          hashmap[s[right]] += 1
+          subString += s[right]
+          maxHash = max(hashmap, key=hashmap.get)
+          if len(subString) - hashmap[maxHash] <= k:
+            right += 1
+          else:
+            subString = subString[1:]
+            hashmap[s[left]] -= 1
+            left += 1
+            right += 1
+          counter = max(len(subString),counter)
+
+        return counter
+            
+          
