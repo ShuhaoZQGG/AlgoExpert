24586aeddfde7632d5fedff19410b7c689a67637
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-04-21 23:51:15 -0400

    Time: 107 ms (62.75%), Space: 24.2 MB (31.98%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,29 +8,25 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head:
-            return 
-        
-        # find the middle of linked list [Problem 876]
-        # in 1->2->3->4->5->6 find 4 
-        slow = fast = head
+        slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next 
-            
-        # reverse the second part of the list [Problem 206]
-        # convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
-        # reverse the second half in-place
-        prev, curr = None, slow
-        while curr:
-            curr.next, prev, curr = prev, curr, curr.next       
+            fast = fast.next.next
+        
+        second = slow.next
+        prev = None
+        slow.next = None
+        while second:
+            nextNode = second.next
+            second.next = prev
+            prev = second
+            second = nextNode
 
-        # merge two sorted linked lists [Problem 21]
-        # merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
+        
         first, second = head, prev
-        while second.next:
-            first.next, first = second, first.next
-            second.next, second = first, second.next
-            
-    
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next, second.next = second, tmp1
+            first, second = tmp1, tmp2
+
         
