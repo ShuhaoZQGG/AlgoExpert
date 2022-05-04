03acc419afc084d3f981b4327e8e7e836680c3b4
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-05-03 22:34:55 -0400

    Time: 443 ms (40.75%), Space: 21.9 MB (20.67%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,27 +2,24 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        
-        rows = len(grid)
-        cols = len(grid[0])
         res = 0
         visited = set()
-        def bfs(grid, i, j):
-            q = collections.deque([(i , j)])
-            visited.add((i,j))
-            while q:
-                r, c = q.pop()
-                dirs = [[1,0], [0,1], [0,-1], [-1, 0]]            
-                for dr, dc in dirs:
-                    row = r + dr
-                    col = c + dc
-                    if row < rows and row >= 0 and col < cols and col >= 0 and grid[row][col] == '1' and (row, col) not in visited:
-                        visited.add((row, col))
-                        q.append((row, col))
-                 
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == '1' and (i, j) not in visited:
-                    bfs(grid, i, j)
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c) not in visited:
                     res += 1
+                    stack = [(r, c)]
+                    visited.add((r,c))
+                    dirs = [[1,0],[0,1],[-1,0],[0,-1]]
+                    while stack:
+                        r0, c0 = stack.pop()
+                        for dr, dc in dirs:
+                            row = r0 + dr
+                            col = c0 + dc
+                            if row in range(rows) and col in range(cols) and grid[row][col] == '1' and (row, col) not in visited:
+                                stack.append((row,col))
+                                visited.add((row,col))
         return res
