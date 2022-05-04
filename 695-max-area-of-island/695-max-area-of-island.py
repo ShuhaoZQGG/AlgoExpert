067ad20ba8418f2d795e1355b707b603bc362616
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-05-03 22:46:35 -0400

    Time: 184 ms (51.44%), Space: 14.4 MB (84.54%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,25 +1,35 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        res = 0
-        visited = set()
+        if not grid:
+            return 0
+        
         rows = len(grid)
         cols = len(grid[0])
-        
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1 and (r,c) not in visited:
+        curr = 0
+        res = 0
+        visited = set()
+        def bfs(grid, i, j):
+            nonlocal res
+            nonlocal curr
+            curr = 0
+            q = collections.deque([(i , j)])
+            visited.add((i,j))
+            while q:
+                curr += 1
+                r, c = q.pop()
+                dirs = [[1,0], [0,1], [0,-1], [-1, 0]]            
+                for dr, dc in dirs:
+                    row = r + dr
+                    col = c + dc
+                    if row < rows and row >= 0 and col < cols and col >= 0 and grid[row][col] == 1 and (row, col) not in visited:
+                        visited.add((row, col))
+                        q.append((row, col))
+                    res = max(curr, res)
+
+                 
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1 and (i, j) not in visited:
                     curr = 0
-                    stack = [(r, c)]
-                    visited.add((r,c))
-                    dirs = [[1,0],[0,1],[-1,0],[0,-1]]
-                    while stack:
-                        r0, c0 = stack.pop()
-                        curr += 1
-                        for dr, dc in dirs:
-                            row = r0 + dr
-                            col = c0 + dc
-                            if row in range(rows) and col in range(cols) and grid[row][col] == 1 and (row, col) not in visited:
-                                stack.append((row,col))
-                                visited.add((row,col))
-                            res = max(res, curr)
+                    bfs(grid, i, j)
         return res
