9a6b31584b75a9ae39a276ab2835e25528ec7005
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-06-15 16:49:09 -0400

    Time: 218 ms (63.02%), Space: 15.5 MB (63.75%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,10 +4,13 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         lastZeroIndex = 0
+        # Move all non-zeros to the front, and then increment index
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[lastZeroIndex] = nums[i]
                 lastZeroIndex += 1
         
+        # the number of zeros = length - index
+        # fill the number of zeros at the end
         for i in range(lastZeroIndex, len(nums), 1):
             nums[i] = 0
