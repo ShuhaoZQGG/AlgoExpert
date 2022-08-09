9230ba3bf760c53f3dd7e78eb1ede0120cab32ca
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-08-08 22:10:04 -0400

    Time: 106 ms (92.26%), Space: 19.9 MB (21.42%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,7 +1,27 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
-        for string in strs:
-          ans[''.join(sorted(string))].append(string)
+        ans = {}
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            if tuple(count) in ans:
+                ans[tuple(count)].append(s)
+            else:
+                ans[tuple(count)] = [s]
         return ans.values()
-       
+        
+        '''
+        {
+            eat: [eat, tea, ate]
+            tan: [nat, tan]
+            bat: [bat]
+        }
+        
+        iterate through the input list,
+        check if each word matches with any key in the dict
+        if yes, append the word to the value of the key
+        if not, add a new key and append this value
+        * need a check anagram function
+        iterate through the dicitonary and append all the values
+        '''
