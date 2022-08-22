f260b4164c519a0bc22db4a19d5e8614107281c5
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-08-21 22:02:38 -0400

    Time: 631 ms (19.98%), Space: 21.9 MB (20.23%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,18 +3,18 @@ class Solution:
         if not grid:
             return 0
         islands = 0
-        queue = collections.deque()
+        stack = []
         visited = set()
         def dfs(i, j):
-            queue.append((i, j))
+            stack.append((i, j))
             visited.add((i, j))
             dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-            while queue:
-                row, col = queue.popleft()
+            while stack:
+                row, col = stack.pop()
                 for dr, dc in dirs:
                     Row, Col = dr + row, dc + col
                     if Row in range(len(grid)) and Col in range(len(grid[0])) and grid[Row][Col] == '1' and (Row, Col) not in visited :
-                        queue.append((Row, Col))
+                        stack.append((Row, Col))
                         visited.add((Row, Col))
         for i in range(len(grid)):
             for j in range(len(grid[0])):
