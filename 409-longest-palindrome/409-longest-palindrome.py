1b92e858646773b1c3802604365d8793ef5aeefe
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-09-06 22:19:35 -0400

    Time: 44 ms (71.06%), Space: 13.9 MB (22.39%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -2,7 +2,6 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         hm = {}
         ans = 0
-        highOdd = 0
         for c in s:
             if c in hm:
                 hm[c] += 1
@@ -13,6 +12,5 @@ class Solution:
             ans += v // 2 * 2
             if ans % 2 == 0 and v % 2 == 1:
                     ans += 1
-        ans += highOdd
         return ans
 
