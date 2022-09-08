d6b634c50b82f48987ffc9e9a80754028728e9bf
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-09-08 17:54:24 -0400

    Time: 163 ms (9.85%), Space: 14.1 MB (38.39%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,20 +1,15 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        stack = [(sr, sc)]
-        visited = set()
+        R, C = len(image), len(image[0])
         initial = image[sr][sc]
-        row_length = len(image)
-        col_length = len(image[0])
-        while stack:
-            row, col = stack.pop()
-            if image[row][col] == initial:
-                image[row][col] = color
-            visited.add((row, col))
-            for dr, dc in dirs:
-                ROW = dr + row
-                COL = dc + col
-                if ROW in range(row_length) and COL in range(col_length) and (ROW, COL) not in visited and image[ROW][COL] == initial:
-                    stack.append((ROW, COL))
-                    visited.add((ROW, COL))
+        if initial == color: return image
+        def dfs(r, c):
+            if image[r][c] == initial:
+                image[r][c] = color
+                if r >= 1: dfs(r-1, c)
+                if r+1 < R: dfs(r+1, c)
+                if c >= 1: dfs(r, c-1)
+                if c+1 < C: dfs(r, c+1)
+
+        dfs(sr, sc)
         return image
