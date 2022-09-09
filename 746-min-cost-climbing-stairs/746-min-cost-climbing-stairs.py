bc2d7540377a60cb8a46621ef9912c9254bfde20
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-09-08 22:51:49 -0400

    Time: 168 ms (5.09%), Space: 16.9 MB (5.22%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,11 +1,10 @@
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:  
         @cache
         def minimum_cost(i):
             if i <= 1:
                 return 0
-            
-            one_step = cost[i - 1] + minimum_cost(i - 1)
-            two_steps = cost[i - 2] + minimum_cost(i - 2)
-            return min(one_step, two_steps)
+            down_one = cost[i - 1] + minimum_cost(i - 1)
+            down_two = cost[i - 2] + minimum_cost(i - 2)
+            return min(down_one, down_two)
         return minimum_cost(len(cost))
