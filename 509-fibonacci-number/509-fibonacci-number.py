6cdad7e0a733c04d7ccd925ddfbad27343a69a74
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-09-08 21:45:04 -0400

    Time: 35 ms (89.17%), Space: 13.8 MB (55.13%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,15 +1,24 @@
 class Solution:
     def fib(self, n: int) -> int:
-        res = [0, 1]
+        dp = [0, 1]
+        counter = 2
         if n == 0:
-            return 0
+            return dp[n]
+        
         if n == 1:
-            return 1
-        if n == 2:
-            return 1
-        counter = n
-        while counter >= 2:
-            res.append(res[-1] + res[-2])
-            counter -= 1
-        return res[-1]
+            return dp[n]
+        
+        while n >= 2:
+            if counter % 2 == 0:
+                dp[0] = dp[0] + dp[1]
+            else:
+                dp[1] = dp[0] + dp[1]
+            n -= 1
+            counter += 1
+            
+        if counter%2 == 0:
+            return dp[1]
+        else:
+            return dp[0]
+        
         
