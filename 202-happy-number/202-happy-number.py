18e9bdcc15468d4b9b0b960133dd33e79aaab6da
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-09-10 11:28:36 -0400

    Time: 43 ms (79.42%), Space: 13.9 MB (15.11%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,7 +1,8 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         while n != 1:
-            n = sum(int(c) ** 2 for c in str(n))
             if n < 10 and n != 1 and n != 7:
                 return False
+            n = sum(int(c) ** 2 for c in str(n))
+
         return True
