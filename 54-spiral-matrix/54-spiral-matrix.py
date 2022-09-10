5d5ddc9abeab90f05bd60ee023c536efa0adb46a
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-09-10 13:13:56 -0400

    Time: 66 ms (9.06%), Space: 14 MB (33.68%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,17 +1,37 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
-        m, n = len(matrix), len(matrix[0])
-        i, j = 0, -1
-        direction = 1
-        while len(res) != len(matrix) * len(matrix[0]):
-            for _ in range(n):
-                j += direction
-                res.append(matrix[i][j])
-            m -= 1
-            for _ in range(m):
-                i += direction
-                res.append(matrix[i][j])
-            n -= 1
-            direction *= -1
+        minRow = 0
+        maxRow = len(matrix) - 1
+        minCol = 0
+        maxCol = len(matrix[0]) - 1
+        while len(res) < len(matrix) * len(matrix[0]):
+            for i in range(minCol, maxCol + 1, 1):
+                res.append(matrix[minRow][i])
+            minRow += 1
+            
+            if len(res) == len(matrix) * len(matrix[0]):
+                break
+                
+            for i in range(minRow, maxRow + 1, 1):
+                res.append(matrix[i][maxCol])
+            maxCol -= 1
+            
+            if len(res) == len(matrix) * len(matrix[0]):
+                break
+                
+            for i in range(maxCol, minCol - 1, -1):
+                res.append(matrix[maxRow][i])
+            maxRow -= 1
+    
+            if len(res) == len(matrix) * len(matrix[0]):
+                break
+                
+            for i in range(maxRow, minRow - 1, -1):
+                res.append(matrix[i][minCol])
+            minCol += 1
+            
+            if len(res) == len(matrix) * len(matrix[0]):
+                break
+
         return res
