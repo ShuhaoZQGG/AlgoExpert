b3bf38837b48ced5887757c11296995ebd5bd1a4
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-09-12 22:57:43 -0400

    Time: 793 ms (5.09%), Space: 14.2 MB (98.53%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -1,12 +1,12 @@
 class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
         def dfs(i):
-            horizontal, vertical = True, False
+            horizontal = True
             stack = [(0, i)]
             while stack:
                 row, col = stack.pop()
                 if horizontal:
-                    horizontal, vertical = not horizontal, not vertical
+                    horizontal = not horizontal
                     if grid[row][col] == 1:
                         col += 1
                         if col not in range(len(grid[0])) or grid[row][col] == -1 :
@@ -19,8 +19,8 @@ class Solution:
                             return -1
                         if row in range(len(grid)) and col in range(len(grid[0])):
                             stack.append((row, col))
-                elif vertical:
-                    horizontal, vertical = not horizontal, not vertical
+                else:
+                    horizontal = not horizontal
                     row += 1
                     if row in range(len(grid)) and col in range(len(grid[0])):
                         stack.append((row, col))
